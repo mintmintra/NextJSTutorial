80b2c20e9a6a54e14f15b73452859af5b24885ee
mintmintra <mintracss019@gmail.com>
2023-03-03 23:31:08 +0700

    layout

--- pages/products/index.js
+++ pages/products/index.js
@@ -1,12 +1,27 @@
 import Head from "next/head"
-export default function Index(){
+
+export async function getStaticProps(){
+    const res = await fetch("https://dummyjson.com/products?limit=12")
+    const data = await res.json()
+    return{
+        props:{products:data.products}
+    }
+}
+
+export default function Index({products}){
     return(
         <>
         <Head>
             <title>สินค้าทั้งหมด | Mint Mintra</title>
             <meta name="keywords" content="Mint Mintra,ร้านค้า,ขายเสื้อผ้า" />
         </Head>
-            <h1>สินค้าทั้งหมด</h1>
+         <div>
+            {products.map(item=>(
+                <div key={item.id}>
+                    <h2>{item.title}</h2>
+                </div>
+            ))}
+         </div>
         </>
     )
 }
